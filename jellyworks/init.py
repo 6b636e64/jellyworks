50d693e1e3407256a42ecf8b90e57a6ef3e47e60
Sarah Manlove <36863627+smanlove29@users.noreply.github.com>
2018-10-28 14:52:56 -0400

    Update init.py

--- jellyworks/init.py
+++ jellyworks/init.py
@@ -4,6 +4,7 @@ from datetime import timedelta
 # Create a super user to use the admin site.
 from django.contrib.auth.models import User
 from faker import Faker
+from faker.providers import person
 
 from sidehustles.models import publicProfile, Services, User, Reviews
 
