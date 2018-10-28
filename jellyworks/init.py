6244608ecb1e74196124e713661ef32d5e1ce49d
Sarah Manlove <36863627+smanlove29@users.noreply.github.com>
2018-10-28 14:46:56 -0400

    Update init.py

--- jellyworks/init.py
+++ jellyworks/init.py
@@ -5,7 +5,7 @@ from datetime import timedelta
 from django.contrib.auth.models import User
 from faker import Faker
 
-from sidehustles.models import PublicProfile, Services, User, Reviews
+from sidehustles.models import publicProfile, Services, User, Reviews
 
 fake = Faker()
   
