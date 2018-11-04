698cff405ea6afac08b5f77fd433092004b1c08f
suzyballes <43183840+suzyballes@users.noreply.github.com>
2018-11-04 16:16:03 -0500

    Update urls.py

--- jellyworks/sidehustles/urls.py
+++ jellyworks/sidehustles/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('profile/', views.profile, name="profile"),
     path('filtersearch/', views.filtersearch, name="filtersearch"),
-    path('product/', views.ProductView.as_view(), name="product"),
+    path('product/', views.product, name="product"),
     path('about/', views.about, name="about")
 
 ]
