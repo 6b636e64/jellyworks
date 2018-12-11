cc644d909571a2a00427b80dce4670827975e79b
Sarah Manlove <36863627+smanlove29@users.noreply.github.com>
2018-12-11 13:47:57 -0500

    Update settings.py

--- jellyworks/jellyworks/settings.py
+++ jellyworks/jellyworks/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'sidehustles.apps.SidehustlesConfig',
-    'users.apps.UsersConfig'
+    'users.apps.UsersConfig',
+    'django_filters'
 ]
 
 MIDDLEWARE = [
